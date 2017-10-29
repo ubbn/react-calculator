c921fac37e58cc99fcf1e2c6a2c42f6bd9e0ada3
ubbn <ubbn@live.com>
2017-10-29 20:57:24 +0100

    Cleaned boilerplate codes/added keydown handlers

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import CreateReactClass from 'create-react-class'
 import Display from '.Display'
 
-var Calculator = CreateReactClass({
+const CalculatorOperations = {
+  '+': (valueA, valueB) => valueA + valueB,
+  '-': (valueA, valueB) => valueA - valueB,
+  '*': (valueA, valueB) => valueA * valueB,
+  '/': (valueA, valueB) => valueA / valueB,
+  '=': (valueA, valueB) => valueB
+}
 
+var Calculator = CreateReactClass({
   state = {
     value: null,
     valueStr: '0',
@@ -23,6 +30,80 @@ var Calculator = CreateReactClass({
       valueStr: '0'
     })
   },
+
+  // delete a last char
+  clearLastChar() {
+    const { displayValue } = this.state
+
+    this.setState({
+      displayValue: displayValue.substring(0, displayValue.length - 1) || '0'
+    })
+  }
+
+  // do calculation
+  performOperation(nextOperator) {
+    const { value, displayValue, operator } = this.state
+    const inputValue = parseFloat(displayValue)
+
+    if (value == null) {
+      this.setState({
+        value: inputValue
+      })
+    } else if (operator) {
+      const currentValue = value || 0
+      const newValue = CalculatorOperations[operator](currentValue, inputValue)
+
+      this.setState({
+        value: newValue,
+        displayValue: String(newValue)
+      })
+    }
+
+    this.setState({
+      waitingForOperand: true,
+      operator: nextOperator
+    })
+  }
+
+  // key-down handler
+  handleKeyDown = (event) => {
+    let { key } = event
+
+    if (event.ctrlKey || event.metaKey)
+      return
+
+    if (key === 'Enter')
+      key = '='
+
+    if ((/\d/).test(key)) {
+      this.inputDigit(parseInt(key, 10))
+    } else if (key in CalculatorOperations) {
+      this.performOperation(key)
+    } else if (key === '.') {
+      this.inputDot()
+    } else if (key === '%') {
+      this.inputPercent()
+    } else if (key === 'Backspace') {
+      event.preventDefault()
+      this.clearLastChar()
+    } else if (key === 'Clear') {
+      event.preventDefault()
+
+      if (this.state.displayValue !== '0') {
+        this.clearDisplay()
+      } else {
+        this.clearAll()
+      }
+    }
+  }  
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }  
 
   render(){
     const { displayValue } = this.state
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Calculator from './components/Calculator';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Calculator />, document.getElementById('root'));
 registerServiceWorker();
