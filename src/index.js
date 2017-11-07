6a98c1e9562b444e1cb7e233cea55cc3ebbdfa67
ubbn <ubbn@live.com>
2017-11-07 10:28:34 +0100

    Did some cleanup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Calculator from './components/Calculator';
 import BootstrapCalculator from './components/BootstrapCalculator';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <div id="wrapper">
-    <BootstrapCalculator />
-  </div>
-  , document.getElementById('app'));
+  <BootstrapCalculator />
+  , document.getElementById('root'));
 registerServiceWorker();
